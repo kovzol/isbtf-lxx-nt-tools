c5d554d3113e6c06ddc90a08dcccb4983274e0b7
Zoltán Kovács <zoltan@geogebra.org>
2025-07-08 11:02:29 +0200

    Add experimental module bibref

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This project aims at collecting the database entries from ISBTF's LXX-NT
 project and saving them as bibref statements. The project is a work in progress.
+
+To run module_demo.py, you need to have the bibref Python module also.
+You can find it in https://github.com/kovzol/bibref, folder "py".
--- module_demo.py
+++ module_demo.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 from isbtf_lxx_nt.tools import *
+from bibref.tools import *
 
 print(passage_str_list("Lk 1 2"))
 o = get_object("3918")
 print(f'{o["quotation_text"]}: {o["book"]} {o["chapter"]}:{o["verse_start"]}-{o["verse_end"]}')
 print(f'{o["intro_text"]}: {o["intro_book"]} {o["intro_chapter"]}:{o["intro_verse_start"]}-{o["intro_verse_end"]}')
+
+print(getrefs_maxlength("SBLGNT LXX Psalms 2"))
