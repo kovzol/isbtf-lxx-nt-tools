8185441f36cf3d14ba86772a4bedf20c8bf8f393
Zoltán Kovács <zoltan@geogebra.org>
2025-07-09 12:21:01 +0200

    Testing text_n and find_n

--- module_demo.py
+++ module_demo.py
@@ -8,4 +8,7 @@ o = get_object("3918")
 print(f'{o["quotation_text"]}: {o["book"]} {o["chapter"]}:{o["verse_start"]}-{o["verse_end"]}')
 print(f'{o["intro_text"]}: {o["intro_book"]} {o["intro_chapter"]}:{o["intro_verse_start"]}-{o["intro_verse_end"]}')
 
-print(getrefs_maxlength("SBLGNT LXX Psalms 2"))
+# print(getrefs_maxlength("SBLGNT LXX Psalms 2"))
+print(text_n(1, o["quotation_text"]))
+print(find_n(1, "LXX"))
+print(find_n(1, "SBLGNT"))
